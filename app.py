d0af51420c2462a297f6ff99342ea3bce7e2ccda
alexm <alex-m@outlook.com>
2023-01-09 17:48:35 -0800

    Add code for Flask and Flask debug toolbar imports and basic setup

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 # Ioana A Mititean
 # Exercise 19.2 - Flask Madlibs
+
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "ceva_secreta"
+
+debug = DebugToolbarExtension(app)
