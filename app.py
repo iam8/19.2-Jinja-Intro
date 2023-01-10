79b0cb0dc1e21c31a9ba17a70d3df23704ab409c
alexm <alex-m@outlook.com>
2023-01-09 17:59:29 -0800

    Add boilerplate HTML for template

--- app.py
+++ app.py
@@ -8,3 +8,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "ceva_secreta"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def homepage():
+    """
+    Display form for inputting missing words into a Madlibs story.
+    """
+
+    return render_template("story_input.html")
+
+
+@app.route("/story")
+def create_story():
+    """
+    Display final story, with the missing words filled in by the user.
+    """
+
+    return render_template("final_story.html")
