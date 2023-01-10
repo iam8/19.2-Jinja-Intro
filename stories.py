61798e241c5a97284389ec6e0c3ecf7766d29a7e
alexm <alex-m@outlook.com>
2023-01-09 17:32:24 -0800

    Adjust spacing and fixed some minor typos in Story docstring

--- stories.py
+++ stories.py
@@ -7,14 +7,14 @@
 class Story:
     """Madlibs story.
 
-    To  make a story, pass a list of prompts, and the text
+    To make a story, pass a list of prompts, and the text
     of the template.
 
         >>> s = Story(["noun", "verb"],
         ...     "I love to {verb} a good {noun}.")
 
     To generate text from a story, pass in a dictionary-like thing
-    of {prompt: answer, promp:answer):
+    of {prompt: answer, prompt:answer):
 
         >>> ans = {"verb": "eat", "noun": "mango"}
         >>> s.generate(ans)
@@ -39,8 +39,6 @@ class Story:
 
 
 # Here's a story to get you started
-
-
 story = Story(
     ["place", "noun", "verb", "adjective", "plural_noun"],
     """Once upon a time in a long-ago {place}, there lived a
